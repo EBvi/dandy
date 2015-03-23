f40bf9014e80a16f466b10ba262320044a29ae34
EBvi <erobeat@gmail.com>
2015-03-23 23:26:20 +0900

    modify: HTML 파싱이 실패할 경우에도 주소 변경을 시도하도록
    한국어맞춤법 검사기 서버 주소 이전을 하였는데, 이전 주소에 접근시 HTTP 응답 코드를 정상으로 던지기도 하므로, 파싱 실패 때에도 변경 시도하도록 하였다.

--- src/run.rb
+++ src/run.rb
@@ -4,22 +4,31 @@ require 'net/http'
 require 'uri'
 
 # 기본 경로 설정
-home_dir = File.expand_path '~/.dandy'
+$home_dir = File.expand_path '~/.dandy'
 
 # 선택 문장 가져오기
-query_file = File.join home_dir, 'query.txt'
+query_file = File.join $home_dir, 'query.txt'
 query = File.read query_file
 
 # 임시로 사용한 선택 문장을 담은 파일 삭제
-File.delete query_file
+# File.delete query_file
 
 # 부산대 맞춤법/문법 검사기 접속
-uri = URI.parse File.read File.join home_dir, 'uri.txt'
+uri = URI.parse File.read File.join $home_dir, 'uri.txt'
 
 http = Net::HTTP.new uri.host, uri.port
 
 request = Net::HTTP::Post.new uri.request_uri
 request.set_form_data 'text1' => query
+
+def change_uri
+    uri = `curl --silent https://raw.githubusercontent.com/EBvi/dandy/master/src/uri.txt`
+    if uri =~ /^http.*/im
+        File.open((File.join $home_dir, 'uri.txt'), 'w') do |file|
+            file.write uri
+        end
+    end
+end
 
 begin
     response = http.request request
@@ -29,28 +38,24 @@ begin
         source = ($1).force_encoding("utf-8")
     else
         source = "HTML 분석에 실패했습니다."
+        # 네트워크 문제가 아니라 버전의 문제가 발생하는 경우도 있었다
+        change_uri()
     end
 rescue => e
     source = e.message
-
-    # 오프라인일 수도 있고 한국어 맞춤법 주소가 변경되었을 수도 있다
-    uri = `curl --silent https://raw.githubusercontent.com/EBvi/dandy/master/src/uri.txt`
-    if uri =~ /^http.*/im
-        File.open((File.join home_dir, 'uri.txt'), 'w') do |file|
-            file.write uri
-        end
-    end
+    # 여기에 도달했다면 서버나 네트워크 관련 문제일 가능성이 높다
+    change_uri()
 end
 
 # 템플릿 파일 읽기
-template_file = File.join home_dir, 'template.html'
+template_file = File.join $home_dir, 'template.html'
 template = File.read template_file
 
 # 템플릿 채우기
 template.gsub! '{{source}}', source
 
 # 최종 결과 파일에 쓰기
-output_file = File.join home_dir, 'output.html'
+output_file = File.join $home_dir, 'output.html'
 File.open(output_file, 'w') do |file|
     file.write template
 end
